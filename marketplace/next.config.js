13b89abd84cacc86f505b8eb1fabef614faba47c
DecoratedWings <>
2024-01-01 17:32:24 -0500

    add language locals

--- marketplace/next.config.js
+++ marketplace/next.config.js
@@ -11,6 +11,15 @@ const nextConfig = {
       },
     ],
   },
+  reactStrictMode: true,
+  i18n: {
+    // These are all the locales you want to support in
+    // your application
+    locales: ['en-US'],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    defaultLocale: 'en-US',
+  }
 };
 
 module.exports = nextConfig;
